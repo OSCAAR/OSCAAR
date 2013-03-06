02b7df543a0eb955107cdfb0a2fe8f950f04e713
Daniel M. Galdi <dmgaldi@gmail.com>
2013-03-05 22:12:05 -0500

    Added a sys.platform check for homeDir()
    It should still work for os x now and it now works on my machine.

--- Code/differentialPhotometry.py
+++ Code/differentialPhotometry.py
@@ -4,32 +4,66 @@ from oscaar import photometry
 import pyfits
 import numpy as np
 from matplotlib import pyplot as plt
+from time import time
 import os
-plt.ion()
+import datetime
+
+## Inputs to paths, to be replaced with init.par parser
+#regsPath = '../Extras/Examples/20120616/stars2.reg'
+#imagesPath = '../Extras/Examples/20120616/tres1-???.fit'
+#darksPath = '../Extras/Examples/20120616/tres1-???d.fit'
+#flatPath = '../Extras/Examples/20120616/masterFlat.fits'
+#trackPlots = False#True
+#photPlots = False
+#apertureRadius = 4.5    ## Best parameter for this dataset
+#ccdGain = 0.77999997138977051
+#smoothConst = 3
+#trackingZoom = 10
+#ingress = oscaar.ut2jd('2012-06-17;02:59:00') ## Enter ingress and egress in JD
+#egress = oscaar.ut2jd('2012-06-17;05:29:00')
+
+#oscaar.homeDir()
+#os.mkdir('../outputs/' + str(datetime.datetime.now()).split('.')[0].replace(':', '_').replace(' ', '__'))
+#outputPath = '../outputs/' + str(datetime.datetime.now()).split('.')[0].replace(':', '_').replace(' ', '__')
+
 outputPath = '../outputs/oscaarDataBase'
 
-data = oscaar.dataBank()  ## initalize databank for data storage, parse init.par
+#oscaar.cd('Code')
+###Parses init for settings###
+init = open('init.par', 'r').read().splitlines()
+for line in init:
+    if line.split() > 1 and line[0] != '#':
+        inline = line.split(':', 1)
+        inline[0] = inline[0].strip()
+        if inline[0] == 'Path to Dark Frames': darksPath = str(inline[1].split('#')[0].strip()) ##Everything after # on a line in init.par is ignored
+        elif inline[0] == 'Path to Master-Flat Frame': flatPath = str(inline[1].split('#')[0].strip())
+        elif inline[0] == 'Path to data images':  imagesPath = str(inline[1].split('#')[0].strip())
+        elif inline[0] == 'Path to regions file': regsPath = str(inline[1].split('#')[0].strip())
+        elif inline[0] == 'Ingress':  ingress = oscaar.ut2jd(str(inline[1].split('#')[0].strip()))
+        elif inline[0] == 'Egress':  egress = oscaar.ut2jd(str(inline[1].split('#')[0].strip()))
+        elif inline[0] == 'Radius':   apertureRadius = float(inline[1].split('#')[0].strip())
+        elif inline[0] == 'Tracking Zoom':   trackingZoom = float(inline[1].split('#')[0].strip())
+        elif inline[0] == 'CCD Gain':    ccdGain = float(inline[1].split('#')[0].strip())
+        elif inline[0] == 'GUI': gui = inline[1].split('#')[0].strip()
+        elif inline[0] == 'Plot Tracking': trackPlots = True if inline[1].split('#')[0].strip() == 'on' else False
+        elif inline[0] == 'Plot Photometry': photPlots = True if inline[1].split('#')[0].strip() == 'on' else False
+        elif inline[0] == 'Smoothing Constant': smoothConst = float(inline[1].split('#')[0].strip())
+        elif inline[0] == 'Init GUI': initGui = inline[1].split('#')[0].strip()
+
+data = oscaar.dataBank(imagesPath,darksPath,flatPath,regsPath,ingress,egress)  ## initalize databank for data storage
 allStars = data.getDict()               ## Store initialized dictionary
 
 ## Prepare systematic corrections: dark frame, flat field
-meanDarkFrame = oscaar.meanDarkFrame(data.darksPath)
-masterFlat = pyfits.getdata(data.flatPath)
-plottingThings,statusBarAx = oscaar.plottingSettings(data.trackPlots,data.photPlots)   ## Tell oscaar what figure settings to use 
-
+meanDarkFrame = oscaar.meanDarkFrame(darksPath)
+masterFlat = pyfits.open(flatPath)[0].data
+print 'plottingThings'
+plottingThings = oscaar.plottingSettings(trackPlots,photPlots)   ## Tell oscaar what figure settings to use 
+print plottingThings
 for expNumber in range(0,len(data.getPaths())):  ## For each exposure:
     print '\n'+data.getPaths()[expNumber]
-    image = (pyfits.getdata(data.getPaths()[expNumber]) - meanDarkFrame)/masterFlat    ## Open image from FITS file
-    data.storeTime(expNumber,pyfits.getheader(data.getPaths()[expNumber])['JD'])   ## Store time from FITS header
-    if statusBarAx != None and expNumber % 15 == 0: 
-        plt.cla()
-        statusBarAx.set_title('oscaar2.0 is running...')
-        statusBarAx.set_xlim([0,100])
-        statusBarAx.set_xlabel('Percent Complete (%)')
-        statusBarAx.get_yaxis().set_ticks([])
-        statusBarAx.barh([0],[100.0*expNumber/len(data.getPaths())],[1],color='k')
-        
+    image = (pyfits.open(data.getPaths()[expNumber])[0].data - meanDarkFrame)/masterFlat    ## Open image from FITS file
+    data.storeTime(expNumber,pyfits.open(data.getPaths()[expNumber])[0].header['JD'])   ## Store time from FITS header
     for star in allStars:
-        if statusBarAx == None: plt.clf()
         if expNumber == 0:
             est_x = allStars[star]['x-pos'][0]  ## Use DS9 regions file's estimate for the 
             est_y = allStars[star]['y-pos'][0]  ##    stellar centroid for the first exosure
@@ -38,20 +72,13 @@ for expNumber in range(0,len(data.getPaths())):  ## For each exposure:
             est_y = allStars[star]['y-pos'][expNumber-1]    ##    previous exposure centroid as estimate
 
         ## Track and store the stellar centroid
-        x, y, radius, trackFlag = astrometry.trackSmooth(image, est_x, est_y, data.smoothConst, plottingThings, zoom=data.trackingZoom, plots=data.trackPlots)
+        x, y, radius, trackFlag = astrometry.trackSmooth(image, est_x, est_y, smoothConst, plottingThings, zoom=trackingZoom, plots=trackPlots)
         data.storeCentroid(star,expNumber,x,y)
 
         ## Track and store the flux and uncertainty
-        flux, error, photFlag = photometry.phot(image, x, y, data.apertureRadius, plottingThings, ccdGain = data.ccdGain, plots=data.photPlots)
+        flux, error, photFlag = photometry.phot(image, x, y, apertureRadius, plottingThings, ccdGain = ccdGain, plots=photPlots)
         data.storeFlux(star,expNumber,flux,error)
-        
         if trackFlag or photFlag and (not data.getFlag()): data.setFlag(star,False) ## Store error flags
-        if data.trackPlots or data.photPlots: plt.draw()   
-    if statusBarAx != None and expNumber % 15 == 0: plt.draw()
-plt.close()
-plt.ioff()
-fig = plt.figure(num=None, figsize=(10, 8), facecolor='w',edgecolor='k')
-fig.canvas.set_window_title('oscaar2.0') 
 
 times = data.getTimes()
 
@@ -63,7 +90,7 @@ data.scaleFluxes()
 data.calcChiSq()
 chisq = data.getAllChiSq()
 
-meanComparisonStar, meanComparisonStarError = data.calcMeanComparison(ccdGain = data.ccdGain)
+meanComparisonStar, meanComparisonStarError = data.calcMeanComparison(ccdGain = ccdGain)
 lightCurve = data.lightCurve(meanComparisonStar)
 
 binnedTime, binnedFlux, binnedStd = oscaar.medianBin(times,lightCurve,10)
@@ -73,15 +100,17 @@ print np.mean(photonNoise[data.outOfTransit()])
 
 #data.save(outputPath)
 oscaar.save(data,outputPath)
+print 'plotting'
 plt.plot(times,lightCurve,'k.')
 plt.plot(times[data.outOfTransit()],photonNoise[data.outOfTransit()]+1,'b',linewidth=2)
 plt.plot(times[data.outOfTransit()],1-photonNoise[data.outOfTransit()],'b',linewidth=2)
 plt.errorbar(binnedTime, binnedFlux, yerr=binnedStd, fmt='rs-', markersize=6,linewidth=2)
-plt.axvline(ymin=0,ymax=1,x=data.ingress,color='k',ls=':')
-plt.axvline(ymin=0,ymax=1,x=data.egress,color='k',ls=':')
+plt.axvline(ymin=0,ymax=1,x=ingress,color='k',ls=':')
+plt.axvline(ymin=0,ymax=1,x=egress,color='k',ls=':')
 plt.title('Light Curve')
 plt.xlabel('Time (JD)')
 plt.ylabel('Relative Flux')
+print 'showing'
 plt.show()
 
 #for key in data.getKeys():
