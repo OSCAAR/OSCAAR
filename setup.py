3c889da0ce1cdd6e5155876152c61e8cf1dcd85a
bmorris3 <brettmorris21@gmail.com>
2013-05-23 10:46:23 -0400

    Setup indentations homogenized, removed exoplanet databases from exoplanets.org

--- setup.py
+++ setup.py
@@ -1,14 +1,15 @@
 # -*- coding: utf-8 -*-
 
 """ Made by Luuk Visser, University of Leiden and Delft University of 
-    Technology. Initial version (5-19-2013). """
-    
+	Technology. Initial version (5-19-2013). """
+	
 import os
 import sys
 import shutil
 from setuptools import setup
 import subprocess
 import atexit
+from glob import glob 
 
 """ Checking for all required packages and if these are recent enough """
 minimum_numpy_version = "1.6"
@@ -20,130 +21,140 @@ sysplatform = sys.platform
 
 """ Currently only Python 2.7.x is supported """
 if sys.version_info[:2] != (2, 7):
-    raise RuntimeError("must use python 2.7.x")
+	raise RuntimeError("must use python 2.7.x")
 
 try:
-    import numpy as np
+	import numpy as np
 except:
-    raise RuntimeError("Numpy not found")
+	raise RuntimeError("Numpy not found")
 if np.__version__ < minimum_numpy_version:
-    print("*Error*: NumPy version is lower than needed: %s < %s" %
-          (np.__version__, minimum_numpy_version))
-    sys.exit(1)
+	print("*Error*: NumPy version is lower than needed: %s < %s" %
+		  (np.__version__, minimum_numpy_version))
+	sys.exit(1)
 
 try:
-    import scipy
+	import scipy
 except:
-    raise RuntimeError("Scipy not found")
+	raise RuntimeError("Scipy not found")
 if scipy.__version__ < minimum_scipy_version:
-    print("*Error*: Scipy version is lower than needed: %s < %s" %
-          (scipy.__version__, minimum_scipy_version))
-    sys.exit(1)  
-    
+	print("*Error*: Scipy version is lower than needed: %s < %s" %
+		  (scipy.__version__, minimum_scipy_version))
+	sys.exit(1)  
+	
 try:
-    import matplotlib
+	import matplotlib
 except:
-    raise RuntimeError("matplotlib not found")
+	raise RuntimeError("matplotlib not found")
 if matplotlib.__version__ < minimum_matplotlib_version:
-    print("*Error*: matplotlib version is lower than needed: %s < %s" %
-          (matplotlib.__version__, minimum_matplotlib_version))
-    sys.exit(1)
-    
+	print("*Error*: matplotlib version is lower than needed: %s < %s" %
+		  (matplotlib.__version__, minimum_matplotlib_version))
+	sys.exit(1)
+	
 try:
-    import pyfits
+	import pyfits
 except:
-    raise RuntimeError("PyFITS not found")
+	raise RuntimeError("PyFITS not found")
 if pyfits.__version__ < minimum_pyfits_version:
-    print("*Error*: PyFITS version is lower than needed: %s < %s" %
-          (pyfits.__version__, minimum_pyfits_version))
-    sys.exit(1)  
-    
+	print("*Error*: PyFITS version is lower than needed: %s < %s" %
+		  (pyfits.__version__, minimum_pyfits_version))
+	sys.exit(1)  
+	
 try:
 	if sysplatform == 'darwin' or 'linux2':
 		import wx
 	else: # (assuming in this case that sysplatform == 'win32'):
 		import wxPython
 except:
-    raise RuntimeError("wxPython not found")
+	raise RuntimeError("wxPython not found")
 if wx.__version__ < minimum_wxpython_version:
-    print("*Error*: wxPython version is lower than needed: %s < %s" %
-          (wx.__version__, minimum_wxpython_version))
-    sys.exit(1)  
+	print("*Error*: wxPython version is lower than needed: %s < %s" %
+		  (wx.__version__, minimum_wxpython_version))
+	sys.exit(1)  
   
 """ Walk through the subdirs and add all non-python scripts to MANIFEST.in """
-def create_manifest():    
-    print 'creating manifest.in'
-    matches = []
-    for root, dirnames, filenames in os.walk('oscaar'):
-        if ('.git' in str(root)) == False:
-            for filename in filenames:
-                if filename.endswith(('.py', '.pyc')) == False:
-                  matches.append(os.path.join(root, filename))
-    
-    with open('MANIFEST.in', 'w') as f:
-        f.writelines(("include %s\n" % l.replace(' ','?') for l in matches))
-    
+def create_manifest():	
+	print 'creating manifest.in'
+	matches = []
+	for root, dirnames, filenames in os.walk('oscaar'):
+		if ('.git' in str(root)) == False:
+			for filename in filenames:
+				if filename.endswith(('.py', '.pyc')) == False:
+				  matches.append(os.path.join(root, filename))
+				  
+	#print 'FILES:',map(os.path.abspath,glob(os.path.join('.','*.txt')))
+	#for root_plus_filename in map(os.path.abspath,glob(os.path.join('.','*.txt'))):
+	#	matches.append(root_plus_filename)
+	#	print 'TEXT FILES:',root_plus_filename
+		
+	matches.append(os.path.join(os.path.dirname(__file__),'post_setup.py'))
+	with open('MANIFEST.in', 'w') as f:
+		f.writelines(("include %s\n" % l.replace(' ','?') for l in matches))
+
 """ To remove the manifest file after installation """
 def delete_manifest():
-    if os.path.exists('MANIFEST.in'):
-        os.remove('MANIFEST.in')
-    
+	if os.path.exists('MANIFEST.in'):
+		os.remove('MANIFEST.in')
+	
 """ Create list for Python scripts directories to include """
 def get_packages():
-    print 'searching for packages'
-    matches = []
-    for root, dirnames, filenames in os.walk('oscaar'):
-        if ('.git' in str(root)) == False:
-            for filename in filenames:
-                if filename.endswith(('.py', '.pyc')) == True:
-                    matches.append(root)
-    matches = list(set(matches))
-    return matches
-    
+	print 'searching for packages'
+	matches = []
+	for root, dirnames, filenames in os.walk('oscaar'):
+		if ('.git' in str(root)) == False:
+			for filename in filenames:
+				if filename.endswith(('.py', '.pyc')) == True:
+					matches.append(root)
+	matches = list(set(matches))
+	return matches
+	
 def del_dir(dirname):
-    if os.path.exists(dirname):
-        shutil.rmtree(dirname)
-    
+	if os.path.exists(dirname):
+		shutil.rmtree(dirname)
+	
 """ The setup configuration for installing OSCAAR """
 def setup_package():
-    create_manifest()
-    setup(
-        name = "OSCAAR",
-        version = "2.0prebeta",
-        author = "Core Developer: Brett Morris. Contributors: Daniel Galdi, Nolan Matthews, Sam Gross, Luuk Visser",
-        author_email = "oscaarUMD@gmail.com",
-        description = ("oscaar is an open source project aimed at helping you begin to study transiting extrasolar planets with differential photometry."),
-        license = 'LICENSE',
-        keywords = "oscaar transit astronomy photometry exoplanets",
-        url = "https://github.com/OSCAAR/OSCAAR/wiki",
-        packages=get_packages(),
-        include_package_data = True,
-        zip_safe = False,
-        long_description=open(os.path.join(os.path.dirname(__file__),'oscaar','README')).read(),
-        download_url='https://github.com/OSCAAR/OSCAAR/archive/master.zip',
-        classifiers=[
-          'Development Status :: 4 - Beta',
-          'Intended Audience :: Science/Research',
-          'License :: OSI Approved :: MIT License',
-          'Operating System :: OS Independent',
-          'Programming Language :: C',
-          'Programming Language :: Python :: 2.7',
-          'Topic :: Scientific/Engineering :: Astronomy',
-          'Topic :: Scientific/Engineering :: Physics'
-      ],
-    )
+	create_manifest()
+	setup(
+		name = "OSCAAR",
+		version = "2.0prebeta",
+		author = "Core Developer: Brett Morris. Contributors: Daniel Galdi, Nolan Matthews, Sam Gross, Luuk Visser",
+		author_email = "oscaarUMD@gmail.com",
+		description = ("oscaar is an open source project aimed at helping you begin to study transiting extrasolar planets with differential photometry."),
+		license = 'LICENSE',
+		keywords = "oscaar transit astronomy photometry exoplanets",
+		url = "https://github.com/OSCAAR/OSCAAR/wiki",
+		packages=get_packages(),
+		include_package_data = True,
+		zip_safe = False,
+		long_description=open(os.path.join(os.path.dirname(os.path.abspath(__file__)),'oscaar','README')).read(),
+		download_url='https://github.com/OSCAAR/OSCAAR/archive/master.zip',
+		classifiers=[
+		  'Development Status :: 4 - Beta',
+		  'Intended Audience :: Science/Research',
+		  'License :: OSI Approved :: MIT License',
+		  'Operating System :: OS Independent',
+		  'Programming Language :: C',
+		  'Programming Language :: Python :: 2.7',
+		  'Topic :: Scientific/Engineering :: Astronomy',
+		  'Topic :: Scientific/Engineering :: Physics'
+	  ],
+	)
 
+print 'INSTALL FILE',os.path.join(os.path.dirname(os.path.abspath(__file__)),'INSTALL.txt')
+print 'INSTALL FILE',os.path.dirname(os.path.abspath(__file__))
 def to_do_at_exit():
-    delete_manifest()
-    del_dir('build')
-    if sys.argv[-1] == 'install': 
-        del_dir('dist')
-    del_dir('OSCAAR.egg-info')
-        
-    'Installation finished. Starting DS9 downloader and C code builder\n'
-    subprocess.check_call(['python', 'post_setup.py',sys.argv[-1]])
-    
+	delete_manifest()
+	del_dir('build')
+	del_dir('OSCAAR.egg-info')
+	
+	if sys.argv[-1] == 'install': 
+		del_dir('dist')
+		
+		'Installation finished. Starting DS9 downloader and C code builder\n'
+		subprocess.check_call(['python', 'post_setup.py',sys.argv[-1]])
+	
+		
 atexit.register(to_do_at_exit)
-    
+	
 if __name__ == '__main__':
-    setup_package()
+	setup_package()
