47f4ec293f51c418cafec15211e24fa66344817d
Dharmatej Mikkilineni <jetamrahd@yahoo.com>
2013-06-27 19:50:01 -0700

    Deleting my test message
    -Dharmatej

--- oscaar/oscaarGUI.py
+++ oscaar/oscaarGUI.py
@@ -2030,8 +2030,6 @@ def checkParams(self,list):
 
     return True
 
-# TESTING THE NEW BRANCH
-
 class InvalidParameter(wx.Frame):
 
     def __init__(self, num, parent, id, str='', max='0', columns=2):
