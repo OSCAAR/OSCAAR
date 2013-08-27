e14aba7587fd2faeb9ff0c1db30bbe9418cc6ba5
Dharmatej Mikkilineni <jetamrahd@yahoo.com>
2013-08-27 12:56:56 -0700

    new mock 2

--- docs/conf.py
+++ docs/conf.py
@@ -13,11 +13,14 @@
 
 import sys
 import os
-import mock
- 
+#import mock
+
+class Mock(object):
+    __all__ = []
+
 MOCK_MODULES = ['numpy', 'scipy', 'matplotlib', 'matplotlib.pyplot', 'scipy.interpolate', 'pyfits']
 for mod_name in MOCK_MODULES:
-    sys.modules[mod_name] = mock.Mock()
+    sys.modules[mod_name] = Mock()
 
 ########################################################################
 ## This "mock" setup replaces all of the packages used by OSCAAR with inert placeholders
