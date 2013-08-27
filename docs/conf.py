7fbaa524bdf57b7edfd5ef68284ed4663036a2a1
Dharmatej Mikkilineni <jetamrahd@yahoo.com>
2013-08-27 03:26:25 -0700

    check2

--- docs/conf.py
+++ docs/conf.py
@@ -46,7 +46,7 @@ for mod_name in MOCK_MODULES:
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 # sys.path.insert(0, os.path.abspath('.'))
-
+sys.path.insert(0, os.path.abspath("../../../../../../Python27/Lib/site-packages/pyfits"))
 # -- General configuration -----------------------------------------------------
 
 # If your documentation needs a minimal Sphinx version, state it here.
