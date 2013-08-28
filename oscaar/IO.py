61c056569d051b6b6e8d8f149d1d7c21a8d3bbca
Dharmatej Mikkilineni <jetamrahd@yahoo.com>
2013-08-28 02:14:57 -0700

    added numpydoc and IO changes

--- oscaar/IO.py
+++ oscaar/IO.py
@@ -16,50 +16,58 @@ from shutil import copy
 import os
 
 def cd(a=None):
-    '''
-    :Description:
-        Change to a different directory than the current one.
-    
-    :Parameters:
-        **a** : string
-            Location of the directory to change to.
 
-    .. Note::
-        If `a` is empty, this function will change to the parent directory.
     '''
+    **Description**: Change to a different directory than the current one.
+    
+    Parameters
+    ----------
+    a : string
+        Location of the directory to change to.
+    
+    Notes
+    -----
+    If `a` is empty, this function will change to the parent directory.
+    '''
+
     if a is None:
         os.chdir(os.pardir)
     else:
         os.chdir(str(a))
 
 def cp(a, b):
+
     '''
-    :Description:
-        Copy a file to another location.
+    **Description**: Copy a file to another location.
     
-    :Parameters:
-        **a** : string
-            Path of the file to be copied.
-        **b** : string
-            Location where the file will be copied to. 
+    Parameters
+    ----------
+    a : string
+        Path of the file to be copied.
+    b : string
+        Location where the file will be copied to. 
     '''
+
     copy(str(a),str(b))
 
 def parseRegionsFile(regsPath):
-    '''
-    :Description:
-        Parse a regions file for a set of data.
 
-    :Parameters:
-        **regsPath** : string
-            Location of the regions file to be parsed.
-    
-    :Returns:
-        **init_x_list** : array
-            An array containing the x-values of the parsed file.
-        **init_y_list** : array
-            An array containing the y-values of the parsed file.
     '''
+    **Description**: Parse a regions file for a set of data.
+
+    Parameters
+    ----------
+    regsPath : string
+        Location of the regions file to be parsed.
+    
+    Returns
+    -------
+    init_x_list : array
+        An array containing the x-values of the parsed file.
+    init_y_list : array
+        An array containing the y-values of the parsed file.
+    '''
+
     regionsData = open(regsPath,'r').read().splitlines()
     init_x_list = []
     init_y_list = []
@@ -71,16 +79,18 @@ def parseRegionsFile(regsPath):
     return init_x_list,init_y_list
     
 def save(data,outputPath):
-    '''
-    :Description:
-        Save everything in oscaar.dataBank object <data> to a python pickle using cPickle.
 
-    :Parameters:
-        **data** : string
-            File location of an oscaar.dataBank() object to save.
-        **outputPath** : string
-            Path to which the numpy-pickle will be saved.   
     '''
+    **Description**: Save everything in oscaar.dataBank object <data> to a python pickle using cPickle.
+
+    Parameters
+    ----------
+    data : string
+        File location of an oscaar.dataBank() object to save.
+    outputPath : string
+        Path to which the numpy-pickle will be saved.   
+    '''
+
     if glob(outputPath) > 0 or glob(outputPath+os.sep+'oscaarDataBase.pkl') > 0 or glob(outputPath+'.pkl') > 0: ## Over-write check
         print 'WARNING: could potentially overwrite the most recent oscaarDataBase.pkl'
     
@@ -99,61 +109,58 @@ def save(data,outputPath):
     output.close()
 
 def load(inputPath):
+
     '''
-    :Description:
-        Load everything from a oscaar.dataBank() object in a python pickle using cPickle.
-    
-    :Parameters:
-        **inputPath** : string
-            File location of an oscaar.dataBank() object to save into a pickle.
-    
-    :Returns:
-        **data** : string
-            Path for the saved numpy-pickle.
+    **Description**: Load everything from a oscaar.dataBank() object in a python pickle using cPickle.
+     
+    Parameters
+    ----------
+    inputPath : string
+        File location of an oscaar.dataBank() object to save into a pickle.
+     
+    Returns
+    -------
+    data : string
+        Path for the saved numpy-pickle.
     '''
+
     inputFile = open(inputPath,'rb')
     data = cPickle.load(inputFile)
     inputFile.close()
     return data
 
 def plottingSettings(trackPlots,photPlots,statusBar=True):
+
     '''
-    :Description:
-        Function for handling matplotlib figures across OSCAAR methods.
+    **Description**: Function for handling matplotlib figures across OSCAAR methods.
     
-    :Parameters:
-        **trackPlots** : boolean
-            Used to turn the astrometry plots on and off.
-        **photPlots** : boolean 
-            Used to turn the aperture photometry plots on and off.
-        **statusBar** : boolean, optional
-            Used to turn the status bar on and off.
+    Parameters
+    ----------
+    trackPlots : boolean
+        Used to turn the astrometry plots on and off.
+    photPlots : boolean 
+        Used to turn the aperture photometry plots on and off.
+    statusBar : boolean, optional
+        Used to turn the status bar on and off.
+       
+    Returns
+    -------
+    [fig,subplotsDimensions,photSubplotsOffset] : [figure, int, int]
+        An array with 3 things. The first is the figure object from matplotlib that will be displayed while 
+        OSCAAR is running. The second is the integer value that designates the x and y dimensions of the 
+        subplots within the figure plot. The third is the the number correlating to the location of the 
+        aperture photometry plots, which depends on the values of trackPlots and photPlots.
+    statusBarFig : figure
+        A figure object from matplotlib showing the status bar for completion.
+    statusBarAx : figure.subplot
+        A subplot from a matplotlib figure object that represents what is drawn.
     
-    EXPL
-    ----
-    
-    :Returns:
-        
-        **[fig,subplotsDimensions,photSubplotsOffset]** : [figure, int, int]
-            An array with 3 things. The first is the figure object from matplotlib that will be displayed while 
-            OSCAAR is running.
-            
-            The second is the integer value that designates the x and y dimensions of the subplots
-            within the figure plot.The
-            
-            third is the the number correlating to the location of the aperture photometry
-            plots, which depends on the values 
-            
-            of trackPlots and photPlots.
-        **statusBarFig** : figure
-            A figure object from matplotlib showing the status bar for completion.
-        **statusBarAx** : figure.subplot
-            A subplot from a matplotlib figure object that represents what is drawn.
-    
-    .. Note::
-        This list returned by plottingSettings() should be stored to a variable, and used as an
-        argument in the phot() and trackSmooth() methods.
+    Notes
+    -----
+    This list returned by plottingSettings() should be stored to a variable, and used as an
+    argument in the phot() and trackSmooth() methods.
     '''
+
     if trackPlots or photPlots: 
         plt.ion()
         statusBarFig = 0 
