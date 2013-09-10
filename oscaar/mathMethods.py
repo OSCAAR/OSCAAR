67658dda834112358a0f0e2266eef0965d7bc76b
Matej Stuchlik <mstuchli@redhat.com>
2013-09-10 22:19:22 +0200

    PEP8-ify mathMethods.py

--- oscaar/mathMethods.py
+++ oscaar/mathMethods.py
@@ -1,51 +1,62 @@
-'''oscaar v2.0 
-   Module for differential photometry
-   Developed by Brett Morris, 2011-2013'''
+"""
+oscaar v2.0
+Module for differential photometry
+
+Developed by Brett Morris, 2011-2013
+"""
+
 import numpy as np
 from numpy import linalg as LA
 
 
-def paddedStr(num,pad):
-    '''Return the number num padded with zero-padding of length pad
-    
+def paddedStr(num, pad):
+    """
+    Return the number num padded with zero-padding of length pad
+
     Parameters
     ----------
     num : float
         number to pad
     pad : int
-        number of zeros to pad in front of `num` 
-            
+        number of zeros to pad in front of `num`
+
     Returns
     -------
         The padded number.
-    
-    '''
+    """
+
     strlen = len(str(num))
     lenpad = pad-strlen
     return str((lenpad*'0')+str(num))
 
+
 def ut2jd(ut):
-    '''
+    """
     Convert times from Universal Time (UT) to Julian Date (JD)
-    
+
     Parameters
     ----------
     ut : string
         Time in Universial Time (UT)
-    
+
     Returns
     -------
     jd : float
         Julian Date (JD)
-    '''
+    """
+
     [date, Time] = ut.split(';')
     Time = Time.strip()
     [year, month, day] = date.split('-')
     [hour, min, sec] = Time.split(':')
-    year = int(year); month = int(month); day = int(day)
-    hour = int(hour); min = int(min); sec = float(sec)
+    year = int(year)
+    month = int(month)
+    day = int(day)
+    hour = int(hour)
+    min = int(min)
+    sec = float(sec)
     #years = (int(year) + 4716)*365.25
-    if month == 1 or month == 2: 
+    if month == 1 or month == 2:
         month += 12
         year -= 1
     a = year/100
@@ -59,28 +70,35 @@ def ut2jd(ut):
     jd = years + fracOfDay
     return jd
 
+
 def ut2jdSplitAtT(ut):
-    '''
-    Convert times from Universal Time (UT) to Julian Date (JD), splitting the date and time at the "T"
-    
+    """
+    Convert times from Universal Time (UT) to Julian Date (JD), splitting
+    the date and time at the "T"
+
     Parameters
     ----------
     ut : string
         Time in Universial Time (UT)
-    
+
     Returns
     -------
     jd : float
         Julian Date (JD)
-    '''
+    """
+
     [date, Time] = ut.split('T')
     Time = Time.strip()
     [year, month, day] = date.split('-')
     [hour, min, sec] = Time.split(':')
-    year = int(year); month = int(month); day = int(day)
-    hour = int(hour); min = int(min); sec = float(sec)
+    year = int(year)
+    month = int(month)
+    day = int(day)
+    hour = int(hour)
+    min = int(min)
+    sec = float(sec)
     #years = (int(year) + 4716)*365.25
-    if month == 1 or month == 2: 
+    if month == 1 or month == 2:
         month += 12
         year -= 1
     a = year/100
@@ -94,11 +112,14 @@ def ut2jdSplitAtT(ut):
     jd = years + fracOfDay
     return jd
 
-def regressionScale(comparisonFlux,targetFlux,time,ingress,egress,returncoeffs=False):
-    '''
-    Use a least-squares regression to stretch and offset a comparison star fluxes
-    to scale them to the relative intensity of the target star. Only do this regression
-    considering the out-of-transit portions of the light curve.
+
+def regressionScale(comparisonFlux, targetFlux, time, ingress, egress,
+                    returncoeffs=False):
+    """
+    Use a least-squares regression to stretch and offset a comparison
+    star fluxes to scale them to the relative intensity of the target
+    star. Only do this regression considering the out-of-transit
+    portions of the light curve.
 
     Parameters
     ----------
@@ -107,65 +128,73 @@ def regressionScale(comparisonFlux,targetFlux,time,ingress,egress,returncoeffs=F
 
     targetFlux : numpy.ndarray
         Flux of the target star
-    
+
     time : numpy.ndarray
         List of times for each flux measurement in JD
-    
+
     ingress : float
         Time of ingress (JD, assuming time list is in JD)
-    
+
     egress : float
         Time of egress (JD, assuming time list is in JD)
-            
+
     Returns
     -------
     scaledVector : numpy.ndarray
-        Rescaled version of the comparisonFlux vector using the above described process
-    '''
+        Rescaled version of the comparisonFlux vector using the above
+        described process
+    """
+
     outOfTransit = (time < ingress) + (time > egress)
     regressMatrix = np.vstack([comparisonFlux[outOfTransit]]).T
-    assert len(targetFlux[outOfTransit]) > 0, 'No fluxes marked as "out-of-transit" according to input ingress/egress'
-    m = LA.lstsq(regressMatrix,targetFlux[outOfTransit])[0]
-    scaledVector = m*comparisonFlux 
+    assert len(targetFlux[outOfTransit]) > 0, \
+        'No fluxes marked as "out-of-transit" according to input ingress/egress'
+    m = LA.lstsq(regressMatrix, targetFlux[outOfTransit])[0]
+    scaledVector = m*comparisonFlux
     if returncoeffs:
-        return scaledVector,m
+        return scaledVector, m
     else:
         return scaledVector
 
-def chiSquared(vector1,vector2):
-    '''Return :math:`$\chi^2$` (chi-squared) of two vectors'''
-    return np.sum(np.power(vector1-vector2,2))
 
-def medianBin(time,flux,medianWidth):
-    '''
+def chiSquared(vector1, vector2):
+    """Return :math:`$\chi^2$` (chi-squared) of two vectors"""
+
+    return np.sum(np.power(vector1-vector2, 2))
+
+
+def medianBin(time, flux, medianWidth):
+    """
     Produce median binning of a flux vector
-    
+
     Parameters
     ----------
     time : list or numpy.ndarray
         List of times in time series
-    
+
     flux : list or numpy.ndarray
         List of fluxes, one for each time in `time` vector
 
     medianWidth : int
         Width of each bin in units of data points
-    
+
     Returns
     -------
-    [binnedTime, binnedFlux, binnedStd] : [list, list, list] or [numpy.ndarray, numpy.ndarray, numpy.ndarray]
-        The times, fluxes and uncertainties on each binned point, where `binnedTime` is the time for each 
-        bin, `binnedFlux` is the median flux in each bin, and `binnedStd` is the standard deviation of the points within each bin
-    '''
-    
-    
+    [binnedTime, binnedFlux, binnedStd] : [list, list, list] \
+            or [numpy.ndarray, numpy.ndarray, numpy.ndarray]
+        The times, fluxes and uncertainties on each binned point,
+        where `binnedTime` is the time for each bin, `binnedFlux`
+        is the median flux in each bin, and `binnedStd` is the standard
+        deviation of the points within each bin
+    """
+
     numberBins = len(time)/medianWidth
-    binnedTime = np.arange(numberBins,dtype=float)
-    binnedFlux = np.arange(numberBins,dtype=float)
-    binnedStd = np.arange(numberBins,dtype=float)
-    for i in range(0,numberBins):
+    binnedTime = np.arange(numberBins, dtype=float)
+    binnedFlux = np.arange(numberBins, dtype=float)
+    binnedStd = np.arange(numberBins, dtype=float)
+    for i in range(0, numberBins):
         fluxInBin = flux[i*medianWidth:(i+1)*medianWidth+1]
         binnedTime[i] = np.median(time[i*medianWidth:(i+1)*medianWidth+1])
         binnedFlux[i] = np.median(fluxInBin)
         binnedStd[i] = np.std(fluxInBin)
-    return binnedTime, binnedFlux, binnedStd    
+    return binnedTime, binnedFlux, binnedStd
